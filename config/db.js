8e824ccd32fd2b21dee8f17fef142e6b80e7ea19
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-07-14 09:53:41 +0530

    createPool

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT
 });
 
-connection.connect((err) => {
+connection.getConnection((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
         return;
