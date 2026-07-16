e49659d06106c5537f04410e3413959c69988ffb
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-07-16 09:31:51 +0530

    layout

--- app2.js
+++ app2.js
@@ -1,68 +1,37 @@
 import express from "express";
-import mydb from "./config/db.js";
-import studentRoutes from "./routes/student.routes.js";
-import teacherRoutes from "./routes/teacher.routes.js";
-import addressesRoutes from "./routes/addresses.routes.js";
-import districtsRoutes from "./routes/districts.routes.js";
-import dsdivisionsRoutes from "./routes/dsdivision.routes.js";
-import examsRoutes from "./routes/exams.routes.js";
-import familiesRoutes from "./routes/families.routes.js";
-import gndivisionsRoutes from "./routes/gndivisions.routes.js";
-import gradesRoutes from "./routes/grades.routes.js";
-import housesRoutes from "./routes/houses.routes.js";
-import marksRoutes from "./routes/marks.routes.js";
-import provincesRoutes from "./routes/provinces.routes.js";
-import student_subjectsRoutes from "./routes/student_subjects.routes.js";
-import subject_teacherRoutes from "./routes/subject_teacher.routes.js";
-import subjectsRoutes from "./routes/subjects.routes.js";
+import db from "./config/db.js";
+import layout from "express-ejs-layouts";
 
-const app = express()
-app.set("view engine","ejs");
+const app = express();
 
 app.use(express.json());
-app.use("/api/addresses", addressesRoutes);
-app.use("/api/students", studentRoutes);
-app.use("/api/teachers", teacherRoutes);
-app.use("/api/districts", districtsRoutes);
-app.use("/api/dsdivisions", dsdivisionsRoutes);
-app.use("/api/exams", examsRoutes);
-app.use("/api/families", familiesRoutes);
-app.use("/api/gndivisions", gndivisionsRoutes);
-app.use("/api/grades", gradesRoutes);
-app.use("/api/houses", housesRoutes);
-app.use("/api/marks", marksRoutes);
-app.use("/api/provinces", provincesRoutes);
-app.use("/api/student_subjects", student_subjectsRoutes);
-app.use("/api/subject_teachers", subject_teacherRoutes);
-app.use("/api/subjects", subjectsRoutes);
+app.set("view engine", "ejs");
+
+app.use(layout);
+app.set("layout", "layouts/main");
 
 app.get("/", (req, res) => {
-    res.render("index",
-        {
-            title: "Express",
-            message: "Welcome to Express"
+    res.send("Welcome to the Student API");
+});
+
+app.get("/students", (req, res) => {
+    db.query("SELECT * FROM students", (err, result) => {
+        if (err) throw err;
+        res.render("student/index", { students: result });
+    });
+});
+
+app.get("/students/:id", (req, res) => {
+    db.query(
+        "SELECT * FROM students WHERE id = ?",
+        [req.params.id],
+        (err, result) => {
+            if (err) throw err;
+            res.render("student/show", { student: result[0] });
         }
     );
 });
 
-
-app.get("/students", (req, res) => {
-    mydb.query("SELECT * FROM students", (err, result) => {
-        if (err) throw err;
-        res.render("student/index", { students: result });
-    });
-
-});
-
-app.get("/students/:id", (req, res) => {
-    mydb.query("SELECT * FROM students WHERE id = ?", [req.params.id], (err, result) => {
-        if (err) throw err;
-        res.render("student/show", { student: result[0] });
-    });
-
-});
-
-
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+    console.log("Server is running on port 3000");
 });
